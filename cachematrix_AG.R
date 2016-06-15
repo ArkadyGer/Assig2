144f24d7d8289e4f40d13029a2fe11a30c148d60
ArkadyGer <ArkadyGer@gmail.com>
2016-06-15 14:51:42 +0000

    Add files via upload

---
+++ cachematrix_AG.R
@@ -0,0 +1,19 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## function takes the inverse of a matrix and saves it to cache (variable d)
+
+makeCacheMatrix <- function(x = matrix()) {
+    d<<-solve(c)
+    d
+}
+
+
+## function checks whether the inverse of a matrix is already stored in cache (variable d)
+
+cacheSolve <- function(x) 
+        ## Return a matrix that is the inverse of 'x'
+{    e<-solve(c)
+if (identical(e,d))
+{d}else
+{e}}
